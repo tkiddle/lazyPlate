da01a2b62162d62f1d75762960629714932adfdd
Thomas Kiddle <tkiddle@hotmail.com>
2015-05-02 16:26:27 +0100

    Updating Grunt file to use variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,52 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    banner: {
+      body:  '/*!\n' +
+            ' * <%= pkg.name %>\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * <%= pkg.url %>\n' +
+            ' * @version <%= pkg.version %>\n' +
+            ' * @author <%= pkg.author %>\n' +
+            ' */\n'
+    },
+
     watch: {
       scripts: {
-        files: ['./js/**/*.js', '!./js/build/*.min.js', '!./js/vendor/*.js'], // When these files change - not index.min.js
-        tasks: ['concat:js', 'uglify'] // Run these tasks
+        files: ['./js/**/*.js', '!./js/build/*.min.js', '!./js/vendor/*.js'],
+        tasks: ['concat:js'] // Run these tasks on save
       }
     },
+
     concat: {
       js: {
         src: ['./js/**/*.js', '!./js/build/*.min.js', '!./js/vendor/*.js'],
         dest: './js/build/index.min.js'
+      },
+
+      options: {
+        stripBanners: false,
+        nonull: true,
+        banner: '<%= banner.body %>'
       }
     },
+
     uglify: {
       js: {
         files: {
           './js/build/index.min.js': ['./js/build/index.min.js']
         }
       }
-    },
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask('default', 'watch');
+  grunt.registerTask('build', ['concat', 'uglify']);
 };
